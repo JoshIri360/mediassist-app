7fb958e744298b692df665d208c37b4a7151755e
josef-sho <josephshodunke4@gmail.com>
2024-06-13 22:17:55 +0100

    feat: Update profile link in dropdown menu . This change allows users to easily navigate to their profile page.This commit is a part of those changes and specifically focuses on updating the profile link in the dropdown menu.

--- app/protected/patient/layout.tsx
+++ app/protected/patient/layout.tsx
@@ -114,7 +114,7 @@ export default function DoctorLayout({
                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem><Link to="/profile">Profile</Link></DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>Logout</DropdownMenuItem>
               </DropdownMenuContent>
