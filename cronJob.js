44badfbb44b178b7084cfd2ad2067ed16eaf57f3
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-06-14 18:01:20 +0100

    refactor: Update cronJob.js to use HTTPS for sending reminders

--- cronJob.js
+++ cronJob.js
@@ -2,7 +2,6 @@ const cron = require("node-cron");
 const fetch = require("node-fetch");
 
 const scheduleReminders = async () => {
-
   // cron.schedule("0 0 * * *", async () => {
   //   try {
   //     const response = await fetch(
@@ -20,7 +19,7 @@ const scheduleReminders = async () => {
   try {
     console.log("Sending post req");
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/sendReminders`,
+      `https://mediassist-app.vercel.app/api/sendReminders`,
       {
         method: "POST",
       }
