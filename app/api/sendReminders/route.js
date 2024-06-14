3f4d2547ad3c9ed490fe2a5ce22fe05477b34fc4
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-06-14 18:34:58 +0100

    Refactor Firebase Admin SDK initialization in sendReminders route

--- app/api/sendReminders/route.js
+++ app/api/sendReminders/route.js
@@ -3,19 +3,19 @@ import admin from "firebase-admin";
 import { app } from "@/firebase/config";
 
 // Initialize Firebase Admin SDK
-// if (!admin.apps.length) {
-//   console.log(
-//     "Private Key 1: ",
-//     process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-//   );
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId: process.env.FIREBASE_PROJECT_ID,
-//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-//     }),
-//   });
-// }
+if (!admin.apps.length) {
+  console.log(
+    "Private Key 1: ",
+    process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  );
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
 export async function POST(req) {
   console.log("Post request received");
