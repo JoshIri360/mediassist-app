960961738b79b92f82de83496a248cd34882704b
Olamiposi <tunde-ajayi.olaoluwasijibomi@lmu.edu.ng>
2024-06-15 03:13:23 +0100

    smooth scroll

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,37 +2,44 @@
 import React from "react";
 import { FloatingNav } from "./ui/floating-navbar";
 import { IconHome } from "@tabler/icons-react";
-import { Contact, Fullscreen, BookOpenCheck } from "lucide-react";
+import { Contact, BookOpenCheck } from "lucide-react";
+import { HeroSection } from '@/components/hero';
+import { ProblemSolution } from '@/components/problem-solution';
+import { GlowingStarsTeam } from '@/components/team';
+
 export function FloatingNavDemo() {
   const navItems = [
     {
       name: "Hero",
-      link: "/",
+      link: "#hero",
       icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "Issues",
-      link: "/about",
+      link: "#issues",
       icon: <BookOpenCheck className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "Team",
-      link: "/contact",
-      icon: (
-        <Contact className="h-4 w-4 text-neutral-500 dark:text-white" />
-      ),
+      link: "#team",
+      icon: <Contact className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
   ];
+
   return (
-    <div className="relative  w-full">
+    <div className="relative w-full">
       <FloatingNav navItems={navItems} />
       <DummyContent />
     </div>
   );
 }
+
 const DummyContent = () => {
   return (
     <div>
+      <section id="hero"><HeroSection /></section>
+      <section id="issues"><ProblemSolution /></section>
+      <section id="team"><GlowingStarsTeam /></section>
     </div>
   );
 };
