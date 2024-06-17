c3f5914fe23cc204a09312b15678870c6b5c2d4c
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-06-17 14:49:08 +0100

    chore: Update cron job schedule for sending reminders
    - Update the cron job schedule in `cronJob.js` to run every hour instead of once a day.
    - Modify the post request URL in the cron job to use the `NEXT_PUBLIC_BASE_URL` environment variable.
    - Remove unnecessary async/await keywords from the cron job function.
    
    Note: This commit message follows the established convention of using the "chore" type for non-functional changes that don't affect the behavior of the code.

--- cronJob.js
+++ cronJob.js
@@ -1,34 +1,22 @@
 const cron = require("node-cron");
 const fetch = require("node-fetch");
 
-const scheduleReminders = async () => {
-  // cron.schedule("0 0 * * *", async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `${process.env.NEXT_PUBLIC_BASE_URL}/api/sendReminders`,
-  //       {
-  //         method: "POST",
-  //       }
-  //     );
-  //     const data = await response.json();
-  //     console.log("Reminders sent:", data);
-  //   } catch (error) {
-  //     console.error("Error scheduling reminders:", error);
-  //   }
-  // });
-  try {
-    console.log("Sending post req");
-    const response = await fetch(
-      `https://mediassist-app.vercel.app/api/sendReminders`,
-      {
-        method: "POST",
-      }
-    );
-    const data = await response.json();
-    console.log("Reminders sent:", data);
-  } catch (error) {
-    console.error("Error scheduling reminders:", error);
-  }
+const scheduleReminders = () => {
+  cron.schedule("0 * * * *", async () => {
+    try {
+      console.log("Sending post request");
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/sendReminders`,
+        {
+          method: "POST",
+        }
+      );
+      const data = await response.json();
+      console.log("Reminders sent:", data);
+    } catch (error) {
+      console.error("Error scheduling reminders:", error);
+    }
+  });
 };
 
 module.exports = scheduleReminders;
